dfa2eda00decdb0d066a72f6b2addba5658dccb5
PavithraNS <nspavi1991@gmail.com>
2022-12-05 12:57:39 +0530

    [Pavithra] Adding HelloWorld Project

---
+++ HelloWorldDemo236Batch/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorldDemo236Batch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World");
+            Console.WriteLine("Welcome to RFP-236Batch Fellowship");
+            Console.ReadLine();
+        }
+    }
+}
